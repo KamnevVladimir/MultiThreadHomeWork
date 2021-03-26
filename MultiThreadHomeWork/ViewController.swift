b2d738b4800826b1a0425e9bda52e2bde5bc525c
Kamnev Vladimir <camnev.vov@yandex.ru>
2021-03-26 18:36:34 +0300

    Найден Race Condition

--- MultiThreadHomeWork/ViewController.swift
+++ MultiThreadHomeWork/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        exampleOne()
+        exampleOne()
 //        exampleTwo()
     
     }
@@ -29,14 +29,20 @@ class ViewController: UIViewController {
         var storage: [String] = []
         let concurrentQueue = DispatchQueue(label: "concurrent", attributes: .concurrent)
         
-        concurrentQueue.async {
+        // Race Condition
+        // Две задачи запущены асинхронно на параллельной очереди
+        // Мы можем быть уверены только в том, что задача на добавление ячеек в storage запускается первее задачи,
+        // В которой происходит изменение этих же ячеек в storage
+        // Тут проблема в том, что вторая задача зависит от первой
+        
+        concurrentQueue.sync { // Выполняем первую задачу на очереди синхронно. Тем самым, блокируем ресурс storage до конца выполнения задачи.
             for i in 0...1000 {
                 sleep(1)
                 storage.append("Cell: \(i)")
             }
         }
 
-        concurrentQueue.async {
+        concurrentQueue.async { // Теперь эта задача выполняется строго после первой задачи, которая выполняется на параллельной очереди синхронно
             for i in 0...1000 {
                 storage[i] = "Box: \(i)"
             }
